13ccdc423889128b5dcd085e73d250c3987a02e5
ferna <fernando10tobar@gmail.com>
2020-03-31 13:15:23 +0300

    COVID-19 get pet by status test

--- src/test/java/GetPetTest.java
+++ src/test/java/GetPetTest.java
@@ -6,7 +6,7 @@ public class GetPetTest {
 
     @Test
     public void getPetById() {
-        int id = 0;
+        int id = 1;
         given()
                 .log().all()
                 .baseUri("https://petstore.swagger.io")
@@ -14,14 +14,51 @@ public class GetPetTest {
                 .get("/v2/pet/{id}", id)
                 .then()
                 .log().all()
-                .statusCode(401);
+                .statusCode(200);
 
 
 
     }
 
     @Test
+    public void getPetByStatus() {
+        String status = "sold";
+        given()
+                .log().all()
+                .baseUri("https://petstore.swagger.io")
+                .when ()
+                .get("/v2/pet/findByStatus?status={status}", status)
+                .then()
+                .log().all()
+                .statusCode(200);
+    }
+
+   /* @Test
+    public void createPet() {
+        String pet = "{ \\\"id\\\": 13567, \\\"category\\\": { \\\"id\\\": 0, \\\"name\\\": \\\"string\\\" }, \\\"name\\\": \\\"doggie4\\\", \\\"photoUrls\\\": [ \\\"string\\\" ], \\\"tags\\\": [ { \\\"id\\\": 0, \\\"name\\\": \\\"string\\\" } ], \\\"status\\\": \\\"available\\\"}";
+        given()
+                .log().all()
+                .baseUri("https://petstore.swagger.io")
+                .when ()
+                .post("/v2/pet", "Content-Type", "application/json", pet)
+                .then()
+                .log().all()
+                .statusCode(200);
+    }*/
+}
+
+
+
+
+
+
+
+
+
+
+
+
+  /*  @Test
     public void createPet() {
 
-    }
-}
+    }*/
