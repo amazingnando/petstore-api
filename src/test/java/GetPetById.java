6bca5a77a5389eb52bdb2db0421cca69e7a167f4
ferna <fernando10tobar@gmail.com>
2020-04-09 19:18:32 +0300

    COVID-19 Each test in separate classes, added before and after

---
+++ src/test/java/GetPetById.java
@@ -0,0 +1,86 @@
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.response.ValidatableResponse;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.anyOf;
+import static org.hamcrest.Matchers.is;
+
+public class GetPetById {
+
+    long createdPetId;
+
+    @Before
+    public void before2() {
+        RequestSpecBuilder spec = new RequestSpecBuilder();
+        spec.setBaseUri("https://petstore.swagger.io/v2");
+        spec.addHeader("Content-Type", "application/json");
+        RestAssured.requestSpecification = spec.build();
+    }
+
+    @Before
+    public void before1() {
+        int id = 0;
+        String body = "{\n" +
+                "  \"id\": \""+ id +"\",\n" +
+                "  \"category\": {\n" +
+                "    \"id\": 0,\n" +
+                "    \"name\": \"string\"\n" +
+                "  },\n" +
+                "  \"name\": \"SpikeJr\",\n" +
+                "  \"photoUrls\": [\n" +
+                "    \"string\"\n" +
+                "  ],\n" +
+                "  \"tags\": [\n" +
+                "    {\n" +
+                "      \"id\": 0,\n" +
+                "      \"name\": \"string\"\n" +
+                "    }\n" +
+                "  ],\n" +
+                "  \"status\": \"available\"\n" +
+                "}";
+        ValidatableResponse response = given()
+                .log()
+                .all()
+                .body(body)
+                .when ()
+                .post("/pet")
+                .then()
+                .log()
+                .all()
+                .statusCode(200);
+
+        createdPetId = response.extract().path("id");
+        System.out.println(createdPetId);
+    }
+
+    @Test
+    public void getPetById(){
+        given()
+                .log()
+                .all()
+                .when()
+                .get("/pet/{id}", createdPetId)
+                .then()
+                .log()
+                .all()
+                .body("id", anyOf(is(createdPetId), is("available")))
+                .statusCode(200);
+    }
+    @After
+    public void deleteTest() {
+        given()
+                .log()
+                .all()
+                .when()
+                .delete("/pet/{id}", createdPetId)
+                .then()
+                .log()
+                .all()
+                .body("message", is(String.valueOf(createdPetId)))
+                .statusCode(200);
+    }
+}
---
+++ src/test/java/UpdatePetTest.java
@@ -0,0 +1,108 @@
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.response.ValidatableResponse;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
+
+public class UpdatePetTest {
+
+    long createdPetId;
+
+    @Before
+    public void before2() {
+        RequestSpecBuilder spec = new RequestSpecBuilder();
+        spec.setBaseUri("https://petstore.swagger.io/v2");
+        spec.addHeader("Content-Type", "application/json");
+        RestAssured.requestSpecification = spec.build();
+    }
+
+    @Before
+    public void before1() {
+        int id = 0;
+        String body = "{\n" +
+                "  \"id\": \""+ id +"\",\n" +
+                "  \"category\": {\n" +
+                "    \"id\": 0,\n" +
+                "    \"name\": \"string\"\n" +
+                "  },\n" +
+                "  \"name\": \"SpikeJr\",\n" +
+                "  \"photoUrls\": [\n" +
+                "    \"string\"\n" +
+                "  ],\n" +
+                "  \"tags\": [\n" +
+                "    {\n" +
+                "      \"id\": 0,\n" +
+                "      \"name\": \"string\"\n" +
+                "    }\n" +
+                "  ],\n" +
+                "  \"status\": \"available\"\n" +
+                "}";
+        ValidatableResponse response = given()
+                .log()
+                .all()
+                .body(body)
+                .when()
+                .post("/pet")
+                .then()
+                .log()
+                .all()
+                .statusCode(200);
+
+        createdPetId = response.extract().path("id");
+        System.out.println(createdPetId);
+    }
+
+    @Test
+    public void updatePet () {
+        int id = 0;
+        String body = "{\n" +
+                "  \"id\": \""+ id +"\",\n" +
+                "  \"category\": {\n" +
+                "    \"id\": 0,\n" +
+                "    \"name\": \"string\"\n" +
+                "  },\n" +
+                "  \"name\": \"Max\",\n" +
+                "  \"photoUrls\": [\n" +
+                "    \"string\"\n" +
+                "  ],\n" +
+                "  \"tags\": [\n" +
+                "    {\n" +
+                "      \"id\": 0,\n" +
+                "      \"name\": \"string\"\n" +
+                "    }\n" +
+                "  ],\n" +
+                "  \"status\": \"sold\"\n" +
+                "}";
+
+        given()
+                .log()
+                .all()
+                .body(body)
+                .when()
+                .put("/pet")
+                .then()
+                .log()
+                .all()
+                .body("name", is("Max"))
+                .statusCode(200);
+    }
+
+    @After
+    public void deleteTest() {
+        given()
+                .log()
+                .all()
+                .when()
+                .delete("/pet/{id}", createdPetId)
+                .then()
+                .log()
+                .all()
+                .body("message", is(String.valueOf(createdPetId)))
+                .statusCode(200);
+    }
+
+}
